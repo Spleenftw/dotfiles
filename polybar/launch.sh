2187053289b087f95cbee7c2c41ed0fd2512790e
spleenftw <spleenftw@gmail.com>
2022-10-08 00:50:39 +0200

    update polybar for multiple screen

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
-DIR="$HOME/.config/polybar"
+## Add this script to your wm startup file.
+#DIR="$HOME/.config/polybar"
+#
+## Terminate already running bar instances
+#killall -q polybar
+#
+## Wait until the processes have been shut down
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+#
+## Launch the bar
+#polybar -q main -c "$DIR"/config.ini &
+#
+## IPC settings and test
+#ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-main
+#echo message >/tmp/ipc-main
 
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
-
-# IPC settings and test
-ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-main
-echo message >/tmp/ipc-main
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
